d6298a7d3a498e5d7a0ce869482315d3548b27b3
Meng Zhang <meng@tabbyml.com>
2026-02-27 13:01:47 +0800

    feat: enhance Next.js configuration with asset prefix and base path handling

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from 'next';
 
+const basePath = (process.env.NEXT_PUBLIC_BASE_PATH || '').replace(/\/$/, "");
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: basePath,
+  reactStrictMode: true,
+  trailingSlash: true, // generate index.html
 };
 
 export default nextConfig;
