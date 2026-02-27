cb6bed1b8f8a522e505e71dff234dbaa3bfbf71f
Meng Zhang <meng@tabbyml.com>
2026-02-27 12:55:04 +0800

    feat: update deployment workflow and configure base path for Next.js site

--- site/next.config.ts
+++ site/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH
 };
 
 export default nextConfig;
