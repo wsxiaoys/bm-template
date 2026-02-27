30e79c4b1d95c965af6e50fb01fb831b3ea164e1
Meng Zhang <meng@tabbyml.com>
2026-02-27 12:27:39 +0800

    feat: add Tailwind CSS configuration and utility functions
    - Created components.json for Tailwind CSS setup with ShadCN schema.
    - Added utility functions in utils.ts for class name management using clsx and tailwind-merge.
    - Updated package.json to include necessary dependencies for Tailwind CSS and related libraries.
    - Added postcss.config.ts for PostCSS integration with Tailwind CSS.

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      <div>Next.js on GitHub Pages</div>
-    </main>
-  );
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
